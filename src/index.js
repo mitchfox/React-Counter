86636288ee7ea04d9aef1e05b025d19c9c7b7f9c
Mitch Fox <39552084+mitchfox@users.noreply.github.com>
2019-04-04 13:53:30 +1000

    titlechange

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App1">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
