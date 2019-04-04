d88ffe2753763a5d39c7aa4d0e936e7bb80df579
Mitch Fox <39552084+mitchfox@users.noreply.github.com>
2019-04-05 00:04:42 +1000

    New Buttons and CSS Styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const superincrement = () => {
+    setCount(oldCount => oldCount + 10);
+  };
+  const increment = () => {
+    setCount(oldCount => oldCount + 1);
+  };
+  const decrement = () => {
+    setCount(oldCount => oldCount - 1);
+  };
+  const reset = () => {
+    setCount(oldCount => 0);
+  };
   return (
-    <div className="App1">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="App">
+      <h1>React Liker Counter</h1>
+      <p>Overall Count: {count}</p>
+      <button onClick={superincrement}>Superlike</button>
+      <button onClick={increment}>Like</button>
+      <button onClick={decrement}>Dislike</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
